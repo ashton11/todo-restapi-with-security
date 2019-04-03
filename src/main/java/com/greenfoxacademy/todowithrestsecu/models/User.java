b07d48309bbe4db6f067f81abf45e3191dc08e2c
Tamas Puskas <tamasp1993@live.com>
2019-04-03 15:10:08 +0200

    User class added, currently defining relationship with todo

---
+++ src/main/java/com/greenfoxacademy/todowithrestsecu/models/User.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.todowithrestsecu.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+  @Id
+  @GeneratedValue(strategy= GenerationType.IDENTITY)
+  private long id;
+  private String username;
+  private String password;
+  private String name;
+}
