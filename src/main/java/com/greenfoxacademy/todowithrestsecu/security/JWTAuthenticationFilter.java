4e35456bdabddcfc8565315698ecb2647d4acd79
Tamas Puskas <tamasp1993@live.com>
2019-08-30 23:55:40 +0200

    Feat(JWTAuthenticationFilter) Add jwt creation + Security constants

--- src/main/java/com/greenfoxacademy/todowithrestsecu/security/JWTAuthenticationFilter.java
+++ src/main/java/com/greenfoxacademy/todowithrestsecu/security/JWTAuthenticationFilter.java
@@ -1,20 +1,67 @@
 package com.greenfoxacademy.todowithrestsecu.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.greenfoxacademy.todowithrestsecu.models.User;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import javax.servlet.Filter;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 
-public class JWTAuthenticationFilter implements Filter {
+import static com.greenfoxacademy.todowithrestsecu.security.SecurityConstants.EXPIRATION_TIME;
+import static com.greenfoxacademy.todowithrestsecu.security.SecurityConstants.HEADER_STRING;
+import static com.greenfoxacademy.todowithrestsecu.security.SecurityConstants.SECRET;
+import static com.greenfoxacademy.todowithrestsecu.security.SecurityConstants.TOKEN_PREFIX;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+  private AuthenticationManager manager;
+
+
   public JWTAuthenticationFilter(AuthenticationManager manager) {
+    this.manager = manager;
   }
 
   @Override
-  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+  public Authentication attemptAuthentication(HttpServletRequest req,
+                                              HttpServletResponse res)
+          throws AuthenticationException {
+    try {
+      User credentials = new ObjectMapper().readValue(req.getInputStream(), User.class);
 
+      return manager.authenticate(
+              new UsernamePasswordAuthenticationToken(
+                      credentials.getUsername(),
+                      credentials.getPassword(),
+                      new ArrayList<>()
+              )
+      );
+    } catch (IOException e) {
+      throw new RuntimeException("Something went wrong, please try again!");
+    }
+  }
+
+  @Override
+  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                          FilterChain chain, Authentication authResult)
+          throws IOException, ServletException {
+    String token = JWT.create()
+            .withSubject(((User) authResult.getPrincipal()).getUsername())
+            .withClaim("email", ((User) authResult.getPrincipal()).getEmail())
+            .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+            .sign(Algorithm.HMAC512(SECRET));
+    response.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
   }
 }
---
+++ src/main/java/com/greenfoxacademy/todowithrestsecu/security/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.greenfoxacademy.todowithrestsecu.security;
+
+public class SecurityConstants {
+  public static final String SECRET = "Boogie";
+  public static final long EXPIRATION_TIME = 30000;
+  public static final String TOKEN_PREFIX = "Bearer ";
+  public static final String HEADER_STRING = "AuthToken";
+}
